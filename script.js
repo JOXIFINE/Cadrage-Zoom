59b6708b260ed449142d1ed8fe65c340d8bd8747
JOXIFINE <josephine.constant3@gmail.com>
2026-05-16 13:02:16 +0200

    Refactor image zoom functionality and preload images

--- script.js
+++ script.js
@@ -1,41 +1,139 @@
 const image = document.getElementById("image");
 
+/*
+IMAGE PRINCIPALE : 1440px
+MINIATURE CENTRALE : 180px
+
+1440 / 180 = 8
+=> zoom parfait = x8
+*/
+
+const MAX_ZOOM = 8;
+const MIN_ZOOM = 1;
+
+/* 6 images */
+
 const images = [
-  "1.jpg",
-  "2.jpg",
-  "3.jpg"
+  "1.webp",
+  "2.webp",
+  "3.webp",
+  "4.webp",
+  "5.webp",
+  "6.webp"
 ];
 
 let current = 0;
 let scale = 1;
 
+/* Préchargement */
+
+images.forEach(src => {
+
+  const img = new Image();
+  img.src = src;
+
+});
+
+/* Update */
+
 function updateZoom() {
-  image.style.transform = `scale(${scale})`;
 
-  // Quand le zoom devient énorme
-  if (scale >= 16) {
+  image.style.transform =
+    `scale(${scale})`;
 
-    // image suivante
-    current = (current + 1) % images.length;
+  /* Passage image suivante */
 
-    // changer image
+  if (scale >= MAX_ZOOM) {
+
+    current =
+      (current + 1) % images.length;
+
     image.src = images[current];
 
-    // reset zoom
+    /* Reset propre */
+
     scale = 1;
+
+    image.style.transform =
+      `scale(${scale})`;
   }
 }
+
+/* Zoom desktop */
 
 window.addEventListener("wheel", (e) => {
 
   e.preventDefault();
 
+  const zoomSpeed = 1.03;
+
   if (e.deltaY < 0) {
-    scale *= 1.1;
+
+    scale *= zoomSpeed;
+
   } else {
-    scale /= 1.1;
+
+    scale /= zoomSpeed;
   }
+
+  /* limite minimum */
+
+  scale = Math.max(
+    MIN_ZOOM,
+    scale
+  );
 
   updateZoom();
 
 }, { passive: false });
+
+/* Mobile pinch zoom */
+
+let lastDistance = null;
+
+window.addEventListener("touchmove", (e) => {
+
+  if (e.touches.length === 2) {
+
+    e.preventDefault();
+
+    const dx =
+      e.touches[0].clientX -
+      e.touches[1].clientX;
+
+    const dy =
+      e.touches[0].clientY -
+      e.touches[1].clientY;
+
+    const distance =
+      Math.sqrt(dx * dx + dy * dy);
+
+    if (lastDistance) {
+
+      if (distance > lastDistance) {
+
+        scale *= 1.02;
+
+      } else {
+
+        scale /= 1.02;
+      }
+
+      scale = Math.max(
+        MIN_ZOOM,
+        scale
+      );
+
+      updateZoom();
+    }
+
+    lastDistance = distance;
+  }
+
+}, { passive: false });
+
+window.addEventListener("touchend", () => {
+
+  lastDistance = null;
+
+});
